b682dc59a35b9fae13498c6d892dc4dd63345d50
ojdmh <johamada0604@gmail.com>
2026-01-21 09:54:12 +0000

    ログインするの機能を追加

--- project2/settings.py
+++ project2/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'kakeibo.apps.KakeiboConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
