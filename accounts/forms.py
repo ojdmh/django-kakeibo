e02394b7b46fbb8c935779201cbe887570135d06
ojdmh <johamada0604@gmail.com>
2026-02-04 09:05:30 +0000

    ユーザー情報閲覧・更新機能を追加

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+"""
+    Accounts 更新フォームの定義
+
+    Filename Forms,py
+    Date: 2026.02.04
+    Written By Hamada Jo
+
+"""
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+
+class UserUpdateForm(ModelForm):
+    """ユーザー情報更新フォーム"""
+    class Meta:
+        model=User
+        fields=('last_name','first_name')
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class']='form-control'
