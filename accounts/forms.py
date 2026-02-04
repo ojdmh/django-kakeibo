19fbfafb7a03effafc9e8832a14fe4e688c4b4a3
ojdmh <johamada0604@gmail.com>
2026-02-04 09:57:53 +0000

    ユーザ登録機能を追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,6 +8,7 @@
 """
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class UserUpdateForm(ModelForm):
     """ユーザー情報更新フォーム"""
@@ -19,3 +20,10 @@ class UserUpdateForm(ModelForm):
         super().__init__(*args,**kwargs)
         for field in self.fields.values():
             field.widget.attrs['class']='form-control'
+
+class UserCreateForm(UserCreationForm):
+    """
+        ユーザ作成用のフォーム
+    """
+    class Meta(UserCreationForm.Meta):
+        fields=('username','email')
